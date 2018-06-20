fe0e3e2d90cfbdd7906fec393f75628a8b9ef189
Mike Zaccardo <mike@blockchaintp.com>
2018-06-20 13:55:07 -0400

    TMP checkpoint 2

--- resources/sawtooth/genesis.sh
+++ resources/sawtooth/genesis.sh
@@ -32,15 +32,38 @@ sawset genesis \
   -o genesis.batch
 sawset proposal create \
   -k /etc/sawtooth/keys/validator.priv \
+  sawtooth.consensus.algorithm=poet \
+  sawtooth.poet.report_public_key_pem='-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArMvzZi8GT+lI9KeZiInn
+4CvFTiuyid+IN4dP1+mhTnfxX+I/ntt8LUKZMbI1R1izOUoxJRoX6VQ4S9VgDLEC
+PW6QlkeLI1eqe4DiYb9+J5ANhq4+XkhwgCUUFwpfqSfXWCHimjaGsZHbavl5nv/6
+IbZJL/2YzE37IzJdES16JCfmIUrk6TUqL0WgrWXyweTIoVSbld0M29kToSkMXLsj
+8vbQbTiKwViWhYlzi0cQIo7PiAss66lAW0X6AM7ZJYyAcfSjSLR4guMz76Og8aRk
+jtsjEEkq7Ndz5H8hllWUoHpxGDqLhM9O1/h+QdvTz7luZgpeJ5KB92vYL6yOlSxM
+fQIDAQAB
+-----END PUBLIC KEY-----' \
+  sawtooth.poet.valid_enclave_measurements=$(poet enclave --enclave-module simulator measurement) \
+  sawtooth.poet.valid_enclave_basenames=$(poet enclave --enclave-module simulator basename) \
   sawtooth.validator.batch_injectors=block_info \
   -o config.batch
+poet registration create \
+  -k /etc/sawtooth/keys/validator.priv \
+  --enclave-module simulator \
+  -o poet.batch
+sawset proposal create \
+  -k /etc/sawtooth/keys/validator.priv \
+  sawtooth.poet.target_wait_time=60 \
+  sawtooth.poet.initial_wait_time=120 \
+  sawtooth.publisher.max_batches_per_block=100 \
+  -o poet-settings.batch
 sawadm genesis \
-  genesis.batch config.batch
+  genesis.batch config.batch poet.batch poet-settings.batch
 sawtooth-validator -vv \
   --endpoint tcp://validator.${NETWORK}:8800 \
   --bind component:tcp://eth0:4004 \
   --bind network:tcp://eth0:8800 \
   --peering dynamic \
+  --scheduler parallel \
   --minimum-peer-connectivity 255 \
   --maximum-peer-connectivity 255 \
   --opentsdb-url http://influxdb.${NETWORK}:8086 \
